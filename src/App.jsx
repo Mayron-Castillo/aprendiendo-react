c549c6d4776f5a90e31e77875ddc288f2369a97d
Mayron Castillo <12mayron12@gmail.com>
2025-03-12 09:32:30 -0600

    gestionando estados de carga y errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import BlogPage from './pages/BlogPage';
-import Home from './Home';
 import PostDetails from './pages/PostDetails';
 import SettingsPage from './pages/SettingsPage';
 import ErrorPage from './pages/ErrorPage';
@@ -9,13 +8,13 @@ import ErrorPage from './pages/ErrorPage';
 function App() {
   return (
       <Routes>
-        <Route path="/" element={<><HomePage /><Home /></>} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/blog" element={<BlogPage />} />
         <Route path="/blog/:id" element={<PostDetails />} />
         <Route path="/settings" element={<SettingsPage />} />
         
         <Route path="*" element={<ErrorPage />} />
-        
+
       </Routes>
   );
 }
