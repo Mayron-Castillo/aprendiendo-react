f116252f1b77535d50aba63ae25bf7a0f86b2c3b
Mayron Castillo <12mayron12@gmail.com>
2025-03-12 09:54:55 -0600

    Usando context para compartir datos entre componentes

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,12 +1,18 @@
+import { useContext } from "react"
 import { Link } from "react-router-dom"
+import { UserContext } from "../contexts/user.context"
 
 function HeaderComponent() {
+    const {user} = useContext(UserContext);
     return (
-        <ul className="list">
-            <li><Link className="li" to='/'>Home</Link></li>
-            <li><Link className="li" to='/blog'>Blog</Link></li>
-            <li><Link className="li" to='/settings'>Settings</Link></li>
-        </ul>
+        <section className="background">
+            <h2 className="principal-text">Welcome {user}</h2>
+            <ul className="list">
+                <li><Link className="li" to='/'>Home</Link></li>
+                <li><Link className="li" to='/blog'>Blog</Link></li>
+                <li><Link className="li" to='/settings'>Settings</Link></li>
+            </ul>
+        </section>
     )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,17 @@
 
+import { useContext } from "react"
 import Home from "../Home"
 import HeaderComponent from "../components/HeaderComponent"
+import { UserContext } from "../contexts/user.context"
 
 function HomePage() {
+    const {user} = useContext(UserContext);
+
     return (
         <>
         <HeaderComponent></HeaderComponent>
         <h2>Home Page</h2>
+        <h2>Hola {user}</h2>
 
         <Home/>
         
